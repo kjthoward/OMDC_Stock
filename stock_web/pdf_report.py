65c3b983e4c817af72729d0ff356dc488a9febac
Kieran Howard <kieran.howard@ouh.nhs.uk>
2020-07-27 16:02:46 +0100

    Total Number of pages in PDF reports
    PDF reports now include the total number of pages as well as the current page (e.g "Page 1 of 5")

--- stock_web/pdf_report.py
+++ stock_web/pdf_report.py
@@ -9,14 +9,14 @@ def report_gen(body, title, httpresponse, user):
     styleNormal = styles['Normal']
     styleHeading = styles['Heading1']
     styleHeading.alignment = 1
-
+    total_pages=0
     def head_footer(canvas, doc):
         canvas.saveState()
         P = Paragraph("Report Generated: {}    By: {} - Stock Database V{}".format(datetime.datetime.today().strftime("%d/%m/%Y"), user, __version__),
                       styleNormal)
         w, h = P.wrap(doc.width, doc.bottomMargin)
         P.drawOn(canvas, doc.leftMargin, h)
-        P = Paragraph("Page {}".format(canvas.getPageNumber()),
+        P = Paragraph("Page {} of {}".format(canvas.getPageNumber(), total_pages),
                       styleNormal)
         w, h = P.wrap(doc.width, doc.bottomMargin)
         P.drawOn(canvas, doc.width+doc.leftMargin, h)
@@ -38,4 +38,28 @@ def report_gen(body, title, httpresponse, user):
            id='normal')
     template = PageTemplate(id='table', frames=frame, onPage=head_footer)
     doc.addPageTemplates([template])
+
+    doc.build(table)
+    ######COMPELTE HACK TO PAGE NUMBERS######
+    #Builds the entire document (above), then counts the number of pages
+    #Then rebuilds the entire document but with the new value for total pages to include in footer
+    total_pages=doc.page
+
+    styles = getSampleStyleSheet()
+    styleNormal = styles['Normal']
+    styleHeading = styles['Heading1']
+    styleHeading.alignment = 1
+
+    TABLE=Table(data=body, repeatRows=1)
+    TABLE.setStyle(TableStyle([('FONTSIZE', (0, 0), (-1, -1), 8),
+                               ('ALIGN', (0, 0), (-1, -1), "CENTER")]))
+    table=[]
+    table.append(TABLE)
+    doc = BaseDocTemplate(httpresponse, topMargin=12, bottomMargin=20, pagesize=landscape(A4))
+
+    frame = Frame(doc.leftMargin, doc.bottomMargin, doc.width, doc.height,
+           id='normal')
+    template = PageTemplate(id='table', frames=frame, onPage=head_footer)
+    doc.addPageTemplates([template])
+
     doc.build(table)
