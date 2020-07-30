9efdb8f5952a360e78cb64d12a7e3ea59e2a698b
Kieran Howard <kieran.howard@ouh.nhs.uk>
2020-07-30 13:03:19 +0100

    Changed all date selections to calendar selection
    This change will also prevent invalid dates (e.g Feb 29th 2019) being selected and closes #29

--- stock_web/forms.py
+++ stock_web/forms.py
@@ -1,4 +1,5 @@
 import datetime
+from dateutil.relativedelta import relativedelta
 from django import forms
 from django.db.models import F
 from django.contrib.auth.models import User
@@ -11,14 +12,8 @@ class LoginForm(forms.Form):
 #sets width of all Select2Widget search boxes
 Select2Widget=Select2Widget(attrs={"style": "width:12.5em"})
 
-#custom select date widget (based off of default), allows you to set a custom data range to display
-class MySelectDateWidget(forms.SelectDateWidget):
-    def get_context(self, name, value, attrs):
-        old_state = self.is_required
-        self.is_required = False
-        context = super(MySelectDateWidget, self).get_context(name, value, attrs)
-        self.is_required = old_state
-        return context
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class NewInvForm1(forms.ModelForm):
     reagent=forms.ModelChoiceField(queryset = Reagents.objects.all().exclude(is_active=False).order_by("name"), label="Reagent", widget=Select2Widget)
@@ -49,8 +44,8 @@ class NewInvForm(forms.ModelForm):
                    "project":Select2Widget,
                    "storage":Select2Widget,
                    "lot_no":forms.Textarea(attrs={"style": "height:2em;"}),
-                   "date_rec":MySelectDateWidget(years=range(datetime.datetime.today().year-1,datetime.datetime.today().year+1)),
-                   "date_exp":MySelectDateWidget(years=range(datetime.datetime.today().year-1,datetime.datetime.today().year+20)),
+                   "date_rec":DateInput(attrs={"min":(datetime.datetime.today()-relativedelta(years=1)).strftime("%Y-%m-%d"), "max":datetime.datetime.today().strftime("%Y-%m-%d")}),
+                   "date_exp":DateInput(),
                    "reagent":forms.HiddenInput(),
                    "vol_rec":forms.HiddenInput(),
                    "current_vol":forms.HiddenInput()}
@@ -66,16 +61,18 @@ class NewInvForm(forms.ModelForm):
         self.fields["supplier"].queryset=Suppliers.objects.exclude(name="Internal").exclude(is_active=False)
         self.fields["project"].queryset=Projects.objects.exclude(is_active=False)
         self.fields["storage"].queryset=Storage.objects.exclude(is_active=False)
+
 class NewProbeForm(forms.ModelForm):
     class Meta:
         model = Inventory
         fields = ("reagent", "supplier", "lot_no", "cond_rec", "date_rec", "po", "date_exp", "project", "storage", "vol_rec")
         widgets = {"lot_no":forms.Textarea(attrs={"style": "height:2em;"}),
-                   "date_rec":MySelectDateWidget(years=range(datetime.datetime.today().year-1,datetime.datetime.today().year+1)),
-                   "date_exp":MySelectDateWidget(years=range(datetime.datetime.today().year-1,datetime.datetime.today().year+20)),
+                   "date_rec":DateInput(attrs={"min":(datetime.datetime.today()-relativedelta(years=1)).strftime("%Y-%m-%d"), "max":datetime.datetime.today().strftime("%Y-%m-%d")}),
+                   "date_exp":DateInput(),
                    "reagent":forms.HiddenInput(),
                    "current_vol":forms.HiddenInput(),
-                   "project":Select2Widget}
+                   "project":Select2Widget,
+                   "storage":Select2Widget}
 
     def clean(self):
         super(NewProbeForm, self).clean()
@@ -91,7 +88,7 @@ class NewProbeForm(forms.ModelForm):
 
 class UseItemForm(forms.ModelForm):
     vol_used = forms.IntegerField(min_value=1, label=u"Volume Used (µl)")
-    date_used = forms.DateField(widget=MySelectDateWidget(years=range(datetime.datetime.today().year-1,datetime.datetime.today().year+5)), label=u"Date Used")
+    date_used = forms.DateField(widget=DateInput(attrs={"min":(datetime.datetime.today()-relativedelta(years=1)).strftime("%Y-%m-%d"), "max":(datetime.datetime.today()+relativedelta(years=5)).strftime("%Y-%m-%d")}))
     class Meta:
         model = Inventory
         fields = ("current_vol","date_op", "last_usage")
@@ -111,12 +108,13 @@ class UseItemForm(forms.ModelForm):
             self.add_error("date_used", forms.ValidationError("Date of use occurs in the future"))
 
 class OpenItemForm(forms.ModelForm):
-    date_op = forms.DateField(widget=MySelectDateWidget(years=range(datetime.datetime.today().year-1,datetime.datetime.today().year+5)), label=u"Date Open")
     project=forms.ModelChoiceField(queryset = Projects.objects.order_by("name"), widget=Select2Widget, required=False)
     class Meta:
         model = Inventory
-        fields = ("date_rec",)
-        widgets = {"date_rec":forms.HiddenInput}
+        fields = ("date_rec","date_op")
+        widgets = {"date_rec":forms.HiddenInput,
+                   "date_op":DateInput(attrs={"min":(datetime.datetime.today()-relativedelta(years=1)).strftime("%Y-%m-%d"), "max":(datetime.datetime.today()+relativedelta(years=5)).strftime("%Y-%m-%d")})}
+        labels = {"date_op":"Date Open"}
     def clean(self):
         super(OpenItemForm, self).clean()
         if self.cleaned_data["date_op"]<datetime.datetime.strptime(self.data["date_rec"],"%Y-%m-%d").date():
@@ -125,7 +123,7 @@ class OpenItemForm(forms.ModelForm):
             self.add_error("date_op", forms.ValidationError("Date open occurs in the future"))
 
 class ValItemForm(forms.ModelForm):
-    val_date = forms.DateField(widget=MySelectDateWidget(years=range(datetime.date.today().year-1,datetime.datetime.today().year+5)), label="Validation Date")
+    val_date = forms.DateField(widget=DateInput(attrs={"min":(datetime.datetime.today()-relativedelta(years=1)).strftime("%Y-%m-%d"), "max":(datetime.datetime.today()+relativedelta(years=5)).strftime("%Y-%m-%d")}), label="Validation Date")
     val_run = forms.CharField(max_length=20, widget=forms.TextInput(attrs={"autocomplete": "off"}), label="Validation Run")
     class Meta:
         model = Inventory
@@ -139,7 +137,7 @@ class ValItemForm(forms.ModelForm):
             self.add_error("val_date", forms.ValidationError("Date of validation run occurs in the future"))
 
 class FinishItemForm(forms.ModelForm):
-    date_fin = forms.DateField(widget=MySelectDateWidget(years=range(datetime.datetime.today().year-1,datetime.datetime.today().year+5)), label=u"Date Finished")
+    date_fin = forms.DateField(widget=DateInput(attrs={"min":(datetime.datetime.today()-relativedelta(years=1)).strftime("%Y-%m-%d"), "max":(datetime.datetime.today()+relativedelta(years=5)).strftime("%Y-%m-%d")}), label="Date Finished")
     class Meta:
         model = Inventory
         fields = ("date_op","fin_text","is_op")
